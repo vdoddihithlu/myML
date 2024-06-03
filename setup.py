2dd867e9b7bc0600d9a39328d6bf63feb3e5ec0a
vdoddihithlu <vdoddihithlu@gmail.com>
2024-06-03 14:21:35 -0700

    added correct statement to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ def get_pypackages(file_path:str) -> List[str]:
     with open (file_path) as file_obj:
         pkgs = file_obj.readlines()
         pkglist = [pkg.replace("\n"," ") for pkg in pkgs]
-      #  for pkg in pkgs:
+      #  pkglist = [pkg.strip() for pkg in file_obj.readlines() if pkg.strip() and pkg.strip() != "-e ."]
+      # for pkg in pkgs:
       #      pkg=pkg.replace("/n","")
       #      pkglist.append(pkg)
 
